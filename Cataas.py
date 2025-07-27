66a30908905ecf9cc899a55d334d5311e4d56241
Marina <itmarinaa257@gmal.com>
2025-07-27 21:20:00 +0300

    Создана функция для получения изображения с котиком

--- Cataas.py
+++ Cataas.py
@@ -5,6 +5,20 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'произошла ошика {e}')
+        return None
+
+
+
 window = Tk()
 window.title('Котики')
 window.geometry('600x400')
