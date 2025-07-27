aec20270aebe40dfada18c2c6f58b9ff9faa0829
Marina <itmarinaa257@gmal.com>
2025-07-27 21:14:31 +0300

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -4,3 +4,18 @@ from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+window = Tk()
+window.title('Котики')
+window.geometry('600x400')
+
+lable = Label()
+lable.pack()
+url = 'https://cataas.com/cat'
+img = load.image(url)
+
+if img:
+    lable.config(image=img)
+    lable.image = img
+
+window.mainloop()
