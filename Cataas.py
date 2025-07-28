5dec388fdca20ace2f1fd202dd71acae23de8438
Marina <itmarinaa257@gmal.com>
2025-07-28 14:51:42 +0300

    Теперь каждое изображение открывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -12,40 +12,48 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
-        img.thumbnail((600,480),Image.Resampling.LANCZOS)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'произошла ошибка {e}')
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
 
     if img:
-        lable.config(image=img)
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        lable = Label(new_window, image=img)
+        lable.pack()
         lable.image = img
+
+
+
 
 def exit():
     window.destroy()
+
 
 window = Tk()
 window.title('Котики')
 window.geometry('600x520')
 
-lable = Label()
-lable.pack()
+
 
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
-file_menu =Menu()
-menu_bar.add_cascade(label='Файл')
-menu_bar.add_command(label='Загрузить фото', command=set_image)
-menu_bar.add_command(label='Загрузить фото', command=set_image)
-menu_bar.add_command(label='Загрузить фото', command=set_image)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=exit)
 
 url = 'https://cataas.com/cat'
 
